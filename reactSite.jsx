6988cba4ee4178be2973ac621042e34bb515c45e
luke <25182924+lukemikael@users.noreply.github.com>
2025-03-25 15:17:23 +0800

    wwwwwwwwwwwwwwwwwww

--- reactSite.jsx
+++ reactSite.jsx
@@ -36,10 +36,104 @@ function CartAndDO (){
     </div>)
 }
 
+function CartOrgInfo() {
+return (
+  <div>
+<div class="container-fluid">
+    <div>&nbsp;</div>
+    <h1>Compliance Matrix Dashboard</h1>
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="enter keywords such as bir central office, PCO, fda">
+    </div>
+</div>
+<div>&nbsp;</div>
+<div class="container-fluid">
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOrganizationalInfo" aria-expanded="false" aria-controls="collapseOrganizationalInfo">
+        Organizational Info
+    </button>
+    <div class="collapse" id="collapseOrganizationalInfo">
+        <table class="table">
+          <tbody>
+            <tr>
+              <th scope="row">Email</th>
+              <td>xinfo@immigration.gov.ph</td>
+            </tr>
+            <tr>
+              <th scope="row">Contact Number</th>
+              <td>0916 343 3434</td>
+            </tr>
+            <tr>
+              <th scope="row">Agency Head</th>
+              <td>Agency Head: Atty. Norman G.Tansingco</td>
+            </tr>
+            <tr>
+              <th scope="row">Designation</th>
+              <td>Commissioner of the Bureau of Immigration</td>
+            </tr>
+            <tr>
+              <th scope="row">Residing Region</th>
+              <td>National Capital Region (NCR)</td>
+            </tr>
+            <tr>
+              <th scope="row">Province</th>
+              <td>Metro Manila</td>
+            </tr>
+            <tr>
+              <th scope="row">District</th>
+              <td>Manila, 1st-6th Districts</td>
+            </tr>
+            <tr>
+              <th scope="row">City</th>
+              <td>Manila</td>
+            </tr>
+            <tr>
+              <th scope="row">Address</th>
+              <td>Magallanes Drive. Intramuros, 1002 Manila</td>
+            </tr>       
+          </tbody>
+        </table>
+    </div>
+    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#cartInformation" aria-expanded="false" aria-controls="collapseCartInformation">
+        Cart Information
+    </button>
+    <div class="collapse" id="cartInformation">
+        <table class="table">
+          <tbody>
+            <tr>
+              <th scope="row">CART Email</th>
+              <td>xinfo@immigration.gov.ph</td>
+            </tr>
+            <tr>
+              <th scope="row">CART Contact #</th>
+              <td>0916 343 3434</td>
+            </tr>
+            <tr>
+              <th scope="row">CART Head/Focal Person</th>
+              <td>Agency Head: Atty. Norman G.Tansingco</td>
+            </tr>
+            <tr>
+              <th scope="row">CART Type</th>
+              <td>Commissioner of the Bureau of Immigration</td>
+            </tr>
+            <tr>
+              <th scope="row">CART Order</th>
+              <td>National Capital Region (NCR)</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+
+<div>&nbsp;</div>
+</div>
+</div>
+</div>
+)
+}
+
 function SearchBox() {
   return <h1></h1>;
 }
-     
+
 function Base() {
   return (
    <div>
